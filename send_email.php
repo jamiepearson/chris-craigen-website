953f2cb2e5cd8d86b7b0e1c58cb602a40db672ff
Jamie Pearson <jl_pearson@hotmail.co.uk>
2025-03-23 22:31:50 +0000

    Create send_email.php

---
+++ send_email.php
@@ -0,0 +1,26 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "chrisjcraigen@gmail.com"; // Your email
+    $subject = "New Contact Form Message from $name";
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    $body = "You have received a new message from your website contact form.\n\n";
+    $body .= "Name: $name\n";
+    $body .= "Email: $email\n\n";
+    $body .= "Message:\n$message\n";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Oops! Something went wrong, please try again.";
+    }
+} else {
+    echo "Unauthorized access!";
+}
+?>
